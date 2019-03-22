d3adfe48fe5bed80e2a996a0ff6acc23e1a82468
Jäger <david3355@gmail.com>
2019-03-22 16:06:04 +0100

    Initial commit

---
+++ app/src/main/java/com/jager/risitaslaugh/MainSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.jager.risitaslaugh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_settings);
+    }
+}
